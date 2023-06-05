c2f06ef6b03d6d4a2339aa2f75b7f947ff7797e9
Dingadung <1229jimin@sju.ac.kr>
2023-06-05 13:10:16 +0900

    Sutdy: OSIV와 성능 최적화

---
+++ jpa-study/실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화/jpashop-vjpa1/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service.query;
+
+import jpabook.jpashop.api.OrderApiController;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+
+    private final OrderRepository orderRepository;
+
+    private List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order order : orders) {
+            System.out.println("order ref = " + order + "id = " + order.getId());
+        }
+
+        List<OrderApiController.OrderDto> collect = orders.stream()
+                .map(o -> new OrderApiController.OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+}
